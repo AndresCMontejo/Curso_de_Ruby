37f02573d743eb0107dd147a39ad62a5491252f2
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-13 11:41:48 -0600

    modicacion del archivo operacion1.rb

--- operacion1.rb
+++ operacion1.rb
@@ -14,6 +14,5 @@ contar = nombre.count('A,a,B,b,C,c,D,d,E,e,F,f,G,g,H,h,I,i,J,j,K,k,L,l,M,m,N,n,Ã
 
   
      
-                  #VEREL VIDEO DE OPERADORES ARIMEiCOs                     
-                      
+                  
                      
