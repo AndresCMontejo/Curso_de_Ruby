208ded072dcb84932d6c7a403915ccd2ad996300
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-20 16:42:20 -0600

    Este archivo contiene como utilizar case

---
+++ case.rb
@@ -0,0 +1,24 @@
+print "Introduce una Palabra que desea traducir: \n"
+  palabra = gets.chomp
+  print "Escriba el idioma que desea, Ingles, Frances: \n"
+  idioma = gets.chomp
+ 
+  
+  puts case palabra && idioma
+
+  when Arbol 
+    "Arbol = Tree"
+  when Antartida
+     "Antartida = Antartica"
+  when Antes
+     "Antes = Before"
+  when Andromeda
+     "Andromeda = Andromeda"
+  when Año
+     "Año = Year"
+  when Asqueroso
+     "Asqueroso = Disgusting"
+  when Alacran
+     "Alacran = Scorpion"
+  when Arte
+     "Arte = Art"
