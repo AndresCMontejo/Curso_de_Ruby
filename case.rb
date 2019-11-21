e6677d9a5be6a5eeebc0e5d180c08b5afff8854a
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-21 14:29:50 -0600

    Se modifico el codigo del documento Case, y se trata de como buscar la lada de algun estado de la republica Mexicana

--- case.rb
+++ case.rb
@@ -1,24 +1,43 @@
-print "Introduce una Palabra que desea traducir: \n"
-  palabra = gets.chomp
-  print "Escriba el idioma que desea, Ingles, Frances: \n"
-  idioma = gets.chomp
- 
-  
-  puts case palabra && idioma
+print "Introduce una lada: \n"
+  lada = gets.chomp.to_i
+ # print "Escriba el idioma que desea, Ingles, Frances: \n"
+ # idioma = gets.chomp
 
-  when Arbol 
-    "Arbol = Tree"
-  when Antartida
-     "Antartida = Antartica"
-  when Antes
-     "Antes = Before"
-  when Andromeda
-     "Andromeda = Andromeda"
-  when Año
-     "Año = Year"
-  when Asqueroso
-     "Asqueroso = Disgusting"
-  when Alacran
-     "Alacran = Scorpion"
-  when Arte
-     "Arte = Art"
+  puts case lada
+  when 993 
+    "Tabasco"
+  when 222
+     "Veracruz"
+  when 881
+     "Durango"
+  when 777
+     "CDMX"
+  when 998
+     "Cancun"
+  when 222
+     "Coatzacoalco"
+  when 555
+     "Edo de Mexico"
+  when 999
+     "Chihuahua"
+  else
+   "Lada no encontratada"
+  end 
+   #  puts case palabra == Frances
+ #when Arbol 
+  # "Arbol = Arbre"
+ #when Antartida
+  #  "Antartida = Antarctique"
+ #when Antes
+  #  "Antes = Avant"
+ #when Andromeda
+  #  "Andromeda = Andromède"
+ #when Año
+  #  "Año = Année"
+ #when Asqueroso
+  #  "Asqueroso = Dégoûtant"
+ #when Alacran
+  #  "Alacran = Alacran"
+ #when Arte
+  #  "Arte = Art"
+ #end 
