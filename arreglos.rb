62a102909c873eb321ff311dfdd8d21b00ace091
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-22 15:43:45 -0600

    Segunda forma de agregar un arreglo mas comentarios

--- arreglos.rb
+++ arreglos.rb
@@ -3,15 +3,20 @@
 #"some string".encoding
 #=> #<Encoding:UTF-8>
 #puts "Año"
-xd  = [0, 'Alguna Palabra', false ]
+xd  = [0, 'Alguna Palabra', false ]  #los datos se separan por comas
 xd[3] = "hola"    #ejemplos de como podemos ir aumentando distintos datos.
 xd[4] = 4         #los numeros deben de ser sin comillas    
 xd[5] = 'cinco'   #tambien se puede usar una comilla.
 xd[6] = true      #palabra reservadas del lenguaje.  
 
-puts xd
+puts xd[2]  #puedes mandar a imprimir en general solo llamando el nombre del arreglo, o una posicion especifica
+#agragando los "[]" mas el numero de la posicion.
 
 #los arreglos son una coleccion de datos del mismo tipo, que se almacenan
 #en posiciones consecutivas de memoria y pueden recibir cualquier nombre
 #los arreglos se pueden estructurar de la siguiente forma: arreglo = ['Posicion 0', 'Posicion 1', 'Posicion 2', 'Posicion n....'] 
 #se empieza a contar de la posicion 0 hasta n posiciones.
+
+arreglo2 = %w[0 1 Hola Saludo] #el %W() es para escribir datos sin comas ni comillas, es otra forma de hacerlo
+puts arreglo2[2] 
+
