a0c17bfdd3af3991e707b8504e2f1231ff52b371
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-22 16:17:00 -0600

    Nuevo archivo que explica la iteracion de arreglos con each

---
+++ Iterar_arreglos_con_each.rb
@@ -0,0 +1,7 @@
+# Ciclo = each 
+
+numeros = [10, 9, 8, 7, 6, 5 ,4]
+   numeros.each do |valor|   #se utiliza el metodo "each" en un arreglo y lo convertimos en valor. 
+      puts "Nuestro valor es #{valor}"
+   end
+
