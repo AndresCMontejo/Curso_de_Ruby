8fa76edb05abd071fcb14ffa6db5931db50f404c
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-22 17:08:13 -0600

    Continuar con el calculo de promedio de los datos e investigar sobre la iteracion de each

--- Iterar_arreglos_con_each.rb
+++ Iterar_arreglos_con_each.rb
@@ -2,6 +2,16 @@
 
 numeros = [10, 9, 8, 7, 6, 5 ,4]
    numeros.each do |valor|   #se utiliza el metodo "each" en un arreglo y lo convertimos en valor. 
-      puts "Nuestro valor es #{valor}"
+     # puts "Nuestro valor es #{valor}"
    end
 
+number = %w[8 9 10 11 12 13 14]
+  suma = 0
+number.each_with_index do |numero,lugar| #"arreglo.each_with_index do" nos servira para poder saber en que posicion se encuentran nuestros datos.
+    #tambien el metodo .each_with_index se encarga de iterar nuestros datos. 
+    #puts "En la posicion #{lugar} tenemos #{numero}" #lugar es como nombramos a nuestra variable de posicion, puede colocar cualquier nombre. 
+suma += numero.to_i     
+end
+       puts "El promedio de los datos es: #{suma.to_f / number.length}"
+
+   
