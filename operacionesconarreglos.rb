c008f17378b0a2ae3578455c27d8301fe6f65808
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-24 23:42:47 -0600

    otra forma de hacer join() es usando *comillas

--- operacionesconarreglos.rb
+++ operacionesconarreglos.rb
@@ -2,6 +2,8 @@
 
 mult = %w[1 2 3 4 5 6 7 8 9 10]
 
-puts mult.join(", ")  #join convierte nuestros datos a una cadena, dentro de las comillas
+#puts mult.join(", ")  #join convierte nuestros datos a una cadena, dentro de las comillas
 #podemos colocar cualquier accion que queramos hacer a nuestros datos
- 
+  
+  print mult * ", "  #join("") tambien se puede hacer con * ""
+    
