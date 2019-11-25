067bd39f65f769d3925166f0ab1091f7e25df5c5
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-24 23:37:55 -0600

    se agrego explicacion sobre lo que hace .join

--- operacionesconarreglos.rb
+++ operacionesconarreglos.rb
@@ -1,1 +1,7 @@
-#mirar video 16
+#multiplicador
+
+mult = %w[1 2 3 4 5 6 7 8 9 10]
+
+puts mult.join(", ")  #join convierte nuestros datos a una cadena, dentro de las comillas
+#podemos colocar cualquier accion que queramos hacer a nuestros datos
+ 
