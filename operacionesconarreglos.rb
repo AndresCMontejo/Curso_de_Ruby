9eec4180f0db1382d3c8d6ec1770925caba2923a
AndresCMontejo <andrescmontejo@gmail.com>
2019-11-24 23:50:47 -0600

    como ordenas datos de menor a mayor

--- operacionesconarreglos.rb
+++ operacionesconarreglos.rb
@@ -1,9 +1,12 @@
 #multiplicador
 
-mult = %w[1 2 3 4 5 6 7 8 9 10]
+mult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  #para ordenar hay que dejar de utilizar %w, ya que requerimos datos enteros 
 
 #puts mult.join(", ")  #join convierte nuestros datos a una cadena, dentro de las comillas
 #podemos colocar cualquier accion que queramos hacer a nuestros datos
   
-  print mult * ", "  #join("") tambien se puede hacer con * ""
-    
+  #print mult * ", "  #join("") tambien se puede hacer con * ""
+
+  #ordenas nuestros datos
+puts mult.sort 
+ #mult.sort es para ordenar de menor a mayor nuestros datos
