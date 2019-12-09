bd9d2c2cb48222bb4cfb2e0f6ee7835ec2af2c7e
Andrés Contreras <andrescmontejo@gmail.com>
2019-12-09 15:42:16 -0600

    Matrices en Ruby

--- matrices.rb
+++ matrices.rb
@@ -8,6 +8,21 @@
  
  matriz = Matrix[[2,4,9],[6,4,8],[8,9,9]]
  
-   matriz.each(:diagonal) do |i| #en esta linea iteramos la matriz y mandamos a llamar la diagonal de la matriz 
-    puts i
+   #matriz.each(:diagonal) do |i| #en esta linea iteramos la matriz y mandamos a llamar la diagonal de la matriz 
+   matriz.each(:strict_lower) do |i|   #:strict_upper es para saber los numero arriba de la diagonal 
+    #de nuestra matriza y :Strict_lower para saber los numero debajo de la diagonal de la matriz
+       puts i
    end
+
+   #nota:
+=begin
+   Hay otra forma de saber leer las diagonales en Ruby, matematicamente correcto, y esto se hace
+    de la siguiente manera:
+    require 'matrix' 
+ #[2, 4, 9]
+ #[6, 4, 8]
+ #[8, 9, 9]
+ 
+ matriz = [[2,4,9],[6,4,8],[8,9,9]]
+ puts matriz.diagonal?
+=end
