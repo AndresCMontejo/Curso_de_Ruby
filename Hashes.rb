a797805cbd8910b829358ba58f19000d83e3b94f
Andrés Contreras <andrescmontejo@gmail.com>
2019-12-10 11:31:41 -0600

    Documento sobre hash

--- Hashes.rb
+++ Hashes.rb
@@ -1,1 +1,6 @@
-hash = {"nombre" => "andres", "edad" = 24, 10 => "Diez", () => "Parentesis"} 
+hash = {"nombre" => "andres", "edad" => 24, 10 => "Diez", () => "Parentesis"} 
+puts hash[()]
+#Hash funciona como un diccionario, puedes guardar informacion, e inclusive mandar a imprimir de manera 
+#independiente cada información
+
+#los hashes son conocidos como arrays asosiativos. 
