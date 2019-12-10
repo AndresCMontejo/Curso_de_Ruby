52898acdd468322b694d7bf7648eb03e8b731cac
Andrés Contreras <andrescmontejo@gmail.com>
2019-12-10 17:12:45 -0600

    Operaciones con Hashes

--- operaciones_con_hashes.rb
+++ operaciones_con_hashes.rb
@@ -14,7 +14,16 @@ puts contenido.length
 
 #Tercera Operación
 contenido3 = {informacion1: "7", informacion2: "8", informacion3: "9"}
+contenido3.delete(:informacion2)  #usamos .delete(:informacion2) para eliminar una clave.
 puts contenido3.values
 #Podemos usar un metodo para saber como se llaman las claves dentro de toda nuestra información 
 #utilizando puts contenido3.keys, y si queremos saber el resultado de nuestras claves, se utiliza values
 #en el lugar de keys. 
+
+#Cuarta Operación. 
+contenido4 = {informacion10: 10, informacion21: 11, informacion31: 12}
+      # contenido4.clear   #con esto limpiamos los datos de las claves
+      puts contenido4.key("12")
+       
+       #puts contenido4.empty? #verificamos si las claves de nuestras hash estan vacias.  
+    #si llegaramos a comentar nuestro contenido4.clear, entonces empty? nos marcaria false
---
+++ operaciones_con_hashes2.rb
@@ -0,0 +1,10 @@
+#Cuarta Operación. 
+contenido4 = {informacion10: 10, informacion21: 11, informacion31: 12}
+      # contenido4.clear   #con esto limpiamos los datos de las claves
+      puts contenido4.key("12")
+       
+       #puts contenido4.empty? #verificamos si las claves de nuestras hash estan vacias.  
+    #si llegaramos a comentar nuestro contenido4.clear, entonces empty? nos marcaria false
+
+
+    #VIDEO 22 minuto 4.44
