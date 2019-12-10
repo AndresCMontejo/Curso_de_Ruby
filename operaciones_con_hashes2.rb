730f01b98434b43c7f591a50a024fad600845495
Andrés Contreras <andrescmontejo@gmail.com>
2019-12-10 17:20:05 -0600

    Operaciones con Hashes

--- operaciones_con_hashes2.rb
+++ operaciones_con_hashes2.rb
@@ -1,10 +1,9 @@
 #Cuarta Operación. 
-contenido4 = {informacion10: 10, informacion21: 11, informacion31: 12}
+contenido = {informacion1: "Diez", informacion2: "once", informacion3: 12}
       # contenido4.clear   #con esto limpiamos los datos de las claves
-      puts contenido4.key("12")
+      puts contenido.key(12)
+       #.key("algo") sirve para saber la clave de nuestro dato- 
+ 
        
-       #puts contenido4.empty? #verificamos si las claves de nuestras hash estan vacias.  
-    #si llegaramos a comentar nuestro contenido4.clear, entonces empty? nos marcaria false
 
-
-    #VIDEO 22 minuto 4.44
+       #video 22 minuto 4.44
