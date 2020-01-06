c8a3253c28f04e8f4069a06cb39415fc48f23cdb
Andrés Contreras <andrescmontejo@gmail.com>
2020-01-06 17:16:26 -0600

    agregando def initialize

--- clasesyobjetos.rb
+++ clasesyobjetos.rb
@@ -2,6 +2,10 @@
 
 class Video #para crear una clase, se coloca la primera letra mayuscula, mas la palabra reservada "class"
     attr_accessor :minutes, :title
+
+    def initialize(title)
+        self.title = title 
+    end
     def play
     end
     def pause
@@ -10,9 +14,13 @@ class Video #para crear una clase, se coloca la primera letra mayuscula, mas la 
     end
 #Defnimos los metodos dentro de clase
 end
+video_30_curso_ruby = Video.new("Objetos y Clases")
+puts video_30_curso_ruby.title
+=begin
 video_30_curso_ruby = Video.new
 video_30_curso_ruby.title = "Objetos y clases"
 video_31_curso_ruby = Video.new
 video_31_curso_ruby.title = "Atributos"
 puts video_30_curso_ruby.title
 puts video_31_curso_ruby.title
+=end
