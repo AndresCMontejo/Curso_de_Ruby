287b81d28aa1f5b9555c0b2d876f1a0a07cf1f0c
Andrés Contreras <andrescmontejo@gmail.com>
2020-01-06 16:18:53 -0600

    Archivo Keyword_argument_2

--- Keyword_argument_2.rb
+++ Keyword_argument_2.rb
@@ -1,1 +1,11 @@
-#video 26 3.51 s
+def hola(nombre:"",apellido:"",edad:0,**options) #se definen los argumentos y se da un valor por default
+    if edad > 30
+        puts "Hola Señor #{nombre} #{apellido}"
+        elsif edad < 30
+            puts "Hola Joven #{nombre} #{apellido}"
+    end
+    puts options[:musica_favorita]
+
+end
+hola(nombre:"Andres",apellido:"Contreras", edad:24, color_favorito:"Azul", musica_favorita:"ambient")  
+#se le da valores al argumento
