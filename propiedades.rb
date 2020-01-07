9e9f76acc5eaeaca9981230f43da5f722ae32ee3
Andrés Contreras <andrescmontejo@gmail.com>
2020-01-07 12:29:25 -0600

    Se modifico el nombre de archivos, propiedades.rb

---
+++ propiedades.rb
@@ -0,0 +1,9 @@
+#Ejemplo 1
+class Ejemplos
+    def initialize(ex)
+        @prueba = ex 
+        puts @prueba
+    end
+end
+ejemplo1 = Ejemplos.new("ejemplo 1")
+ejemplo2 = Ejemplos.new("ejemplo 2")
