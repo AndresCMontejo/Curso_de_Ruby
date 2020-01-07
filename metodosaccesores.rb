4e5688bdaf2031648eaeefb7aafb1c15f558f9a8
Andrés Contreras <andrescmontejo@gmail.com>
2020-01-07 16:09:25 -0600

    Métodos Accesores

--- metodosaccesores.rb
+++ metodosaccesores.rb
@@ -2,18 +2,24 @@ class Ciencia
     def initialize(astronomia)
         @galaxias = astronomia 
     end
-    def get_galaxias
-        @galaxias 
-    end
-    def set_galaxias(galaxias)
+    def galaxias # =get_galaxias
+        @galaxias
+    end 
+    #def get_galaxias   #tanto get como set, son utilizados en otros lenguajes, como en java, en ruby podemos hacer dicho metodo de esta manera
+        #@galaxias 
+    #end
+   # def set_galaxias(galaxias)
+    #    @galaxias = galaxias
+    
+    def galaxias=(galaxias)   # = set_galaxias(galaxias)
         @galaxias = galaxias
-
     end
 end
 sistemasolar = Ciencia.new("Planetas")
 sistemasolar2 = Ciencia.new("Planetas 2")
-puts sistemasolar.get_galaxias
-sistemasolar.set_galaxias("Universo 1")
-puts sistemasolar.get_galaxias
+#para Ruby
+puts sistemasolar.galaxias #puts sistemasolar.get_galaxias
+sistemasolar.galaxias = "Universos"#sistemasolar.set_galaxias("Universo 1")
+puts sistemasolar.galaxias #puts sistemasolar.get_galaxias
 
 
