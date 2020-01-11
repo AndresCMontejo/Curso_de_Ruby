05d62eb9360ebf871b79541d05c25a5082358d69
Andrés Contreras <andrescmontejo@gmail.com>
2020-01-10 20:26:06 -0600

    Alcance de metodos

--- alcancedemetodos.rb
+++ alcancedemetodos.rb
@@ -1,15 +1,16 @@
+#Información detallada en video 31, minuto 3.24 
 #publicos -> Public
 #privados -> Private
 #protegidos -> Protected
-class Humano
-    def initialize
+class Humano  #Llamamos una clase padre, posteriormente definimos nuestro metodo publico
+    def initialize  #dentro del constructor se manda a llamar el metodo publico con self.publico 
         self.publico
     end
-    def publico
+    def publico   #cuando no se especifica de que tipo es un metodo, por default son publicos. 
         puts "Soy Público"
     end
 end
 class Tutor < Humano 
 end
 Humano.new
-Tutor.new.publico
+Tutor.new.publico  
