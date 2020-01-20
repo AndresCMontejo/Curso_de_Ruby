74d1a4a8f0386199fe40c28901b1e35210c4c175
Andrés Contreras <andrescmontejo@gmail.com>
2020-01-20 17:05:27 -0600

    Metodos de clases, y como reasignamos una variable de instancia

--- Metodosdeclases.rb
+++ Metodosdeclases.rb
@@ -1,1 +1,31 @@
-#video 32
+#en ruby las clases tambien son objetos. el @esunavariabledeinstancia
+class ClaseEsUnObjeto
+    @nombre_clase = "La clase es un objeto"
+def self.nombre_clase  #self es el objeto de la clase
+    @nombre_clase
+end
+#se puede reasignar otro valor a la variable de instancia como se hace con cualquier otro objeto
+def self.nombre_clase=(nombre_clase)
+    @nombre_clase = nombre_clase
+end
+end
+
+class User
+end
+    User.find()
+=begin
+Otra forma de escribir la sintaxis con Self, es la siguiente. 
+class << self
+    def nombre_clase 
+    @nombre_clase
+end
+def nombre_clase=(nombre_clase)
+    @nombre_clase = nombre_clase
+end
+end
+end
+=end
+
+ClaseEsUnObjeto.nombre_clase = "Modificación de la variable de instancia"
+puts ClaseEsUnObjeto.nombre_clase
+
